bdc7f83912e927f6b17a8c45238d29890966bec1
NanthawatKTY <nanthawat.tae123@gmail.com>
2022-10-18 17:47:50 +0700

    Added error layout tag to restaurant page and 404

---
+++ components/restaurants/Ad.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="ad-container">
+        <h1>Get the best restaurant ad</h1>
+        <button class="btn btn-primary btn-lg">Buy Now</button>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .ad-container{
+        margin: 10rem 0;
+        text-align: center;
+        font-size: 2rem;
+        background-color: rgba(103, 103, 231, 0.3);
+        padding: 3rem;
+    }
+</style>
---
+++ layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <slot name="header" />
+        <div>
+            <img class="img-sad-error" src="/images/sad.png" alt="">
+        </div>
+        <slot name="redirectEl"/>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .img-sad-error {
+        width: 200px;
+        height: 200px;
+        margin: 2rem 0;
+    }
+</style>
